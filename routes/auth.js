b4b9705e7347e0528b62738b068081d40708d08e
panforrest <guoqianp@gmail.com>
2018-04-24 00:49:51 -0400

    add user to turbo backend for real using turbo sdk

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,25 @@ const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
 router.post('/register', function(req, res){
-	res.json({
-		confirmation: 'success',
-		data: req.body  //data: query.body
+	var params = req.body
+    turbo.createUser(params)
+	.then(data => {
+	    res.json({
+	    	confirmation: 'success',
+	    	data: data
+	    })
 	})
+	.catch(err => {
+	    res.json({
+            confirmation: 'success',
+            message: err.message
+	    })
+	})    
+
+	// res.json({
+	// 	confirmation: 'success',
+	// 	data: req.body  //data: query.body
+	// })
 })
 
 module.exports = router
